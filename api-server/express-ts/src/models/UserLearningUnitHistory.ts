8ac93d9c252b655f0ea4a9985921145b150f22e1
on1ystar <tjdwls0607@naver.com>
2021-08-23 01:04:49 +0900

    compleat creating model

--- api-server/express-ts/src/models/UserLearningUnitHistory.ts
+++ api-server/express-ts/src/models/UserLearningUnitHistory.ts
@@ -1,10 +1,10 @@
 export class UserLearningUnitHistory {
-  user_id: number;
-  learning_unit_id: number;
-  contents_id: number;
+  userId: number;
+  learningUnitId: number;
+  contentsId: number;
   counts: number | undefined;
-  latest_learning_at: string | undefined;
-  learning_time: string | undefined;
+  latestLearningAt: string | undefined;
+  learningTime: string | undefined;
   constructor(
     user_id: number,
     learning_unit_id: number,
@@ -13,11 +13,11 @@ export class UserLearningUnitHistory {
     latest_learning_at?: string,
     learning_time?: string
   ) {
-    user_id;
-    learning_unit_id;
-    contents_id;
-    counts;
-    latest_learning_at;
-    learning_time;
+    this.userId = user_id;
+    this.learningUnitId = learning_unit_id;
+    this.contentsId = contents_id;
+    this.counts = counts;
+    this.latestLearningAt = latest_learning_at;
+    this.learningTime = learning_time;
   }
 }
