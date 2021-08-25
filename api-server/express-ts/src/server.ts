637018157690f0e6b041f0520fa88d37a7f88e24
on1ystar <tjdwls0607@naver.com>
2021-08-25 20:37:25 +0900

    complete creating interfaces

--- api-server/express-ts/src/server.ts
+++ api-server/express-ts/src/server.ts
@@ -1,19 +1,25 @@
-import express from 'express';
+import express, { Application } from 'express';
 import helmet from 'helmet';
 import morgan, { Morgan } from 'morgan';
 import cors from 'cors';
-import rootRouter from './routers/rootRouter';
+import indexRouter from './routers';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 
-const app: express.Application = express();
-const logger = morgan('dev');
+const app: Application = express();
 const port: number = Number(process.env.PORT) || 4000;
+const logger = morgan('dev');
+const swaggerSpec = YAML.load(path.join(__dirname, '../build/swagger.yaml'));
 
 app.use(helmet()); // 보안 모듈
 app.use(logger); // 로그 관리 모듈
-app.use(express.urlencoded({ extended: true })); // url query prameter 파싱
+app.use(express.json()); // request body parsing
+app.use(express.urlencoded({ extended: true })); // url query prameter parsing
 app.use(cors()); // cors 모듈
 
-app.use('/', rootRouter); // 루트 라우트
+app.use('/', indexRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec)); // swagger로 작성한 파일 setup
 
 app.listen(port, (): void =>
   // eslint-disable-next-line no-console
